1c33ee0de01c71813a7f79960b5c71afe6c22086
Yuji <yuji.tachikawa@ipmu.jp>
2020-10-25 21:33:31 +0900

    Initial Commit

---
+++ nsstatusitem-lightdark-detect/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  nsstatusitem-lightdark-detect
+//
+//  Created by Yuji on 10/25/20.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ nsstatusitem-lightdark-detect/AppDelegate.m
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.m
+//  nsstatusitem-lightdark-detect
+//
+//  Created by Yuji on 10/25/20.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@property (strong) IBOutlet NSWindow *window;
+@end
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+    // Insert code here to initialize your application
+}
+
+
+- (void)applicationWillTerminate:(NSNotification *)aNotification {
+    // Insert code here to tear down your application
+}
+
+
+@end
