806e1770a3ec03359a6c554df24349b073e950c0
Yuji <yuji.tachikawa@ipmu.jp>
2020-10-25 23:09:38 +0900

    minimal code

--- nsstatusitem-lightdark-detect/AppDelegate.m
+++ nsstatusitem-lightdark-detect/AppDelegate.m
@@ -13,9 +13,35 @@
 @end
 
 @implementation AppDelegate
+{
+    NSMutableArray<NSStatusItem*>* o;
+}
+-(void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary<NSKeyValueChangeKey,id> *)change context:(void *)context
+{
+    if([keyPath isEqualToString:@"button.effectiveAppearance"]){
+        NSStatusItem*item=object;
+        NSAppearance*appearance=item.button.effectiveAppearance;
+        NSString*appearanceName=(NSString*)(appearance.name);
+        if([[appearanceName lowercaseString] containsString:@"dark"]){
+            item.button.title=@"dark";
+        }else{
+            item.button.title=@"light";
+        }
+    }
+}
+-(IBAction)createStatusItem:(id)sender
+{
+    NSStatusItem*item=[[NSStatusBar systemStatusBar] statusItemWithLength:NSVariableStatusItemLength];
+    item.button.title=@"foo";
+    item.visible=YES;
+    [item addObserver:self forKeyPath:@"button.effectiveAppearance" options:NSKeyValueObservingOptionNew|NSKeyValueObservingOptionInitial context:nil];
+    [o addObject:item];
+}
+
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
     // Insert code here to initialize your application
+    o=[NSMutableArray array];
 }
 
 
